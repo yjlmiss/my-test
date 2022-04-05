0ed4e64eeb51c72572ae694f98a2797cf00aa29c
yjlmiss <yjl4022@gmail.com>
2022-04-05 11:04:40 +0800

    初稿

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>HELLOWORLD</h1>
+     <MyButton ></MyButton>
+     <MyButton ></MyButton>
+     <MyButton ></MyButton>
     </div>
   );
 }
+function MyButton (){
+  const [count,setcount]= useState(0)//这啥意思
+  
+  function handleClick(){
+    setcount(count+1)
+  }
+  return (
+  <button onClick={handleClick}> Click {count} times</button>
+  )
+}
 
+// class MyButton extends React.Component{
+//   state={
+//     count:0,
+//   };
+//   handleClick(){
+//     const count = this.state.count;
+//     this.setState({
+//       count:count+1
+//     })
+//   };
+//   render(){
+//     return(
+//       <button onClick={()=>this.handleClick()}>Click {this.state.count} times</button>
+//     )
+    
+    
+//   }
+// }
 export default App;
